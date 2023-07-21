7a8f48a1deb1261f36fa6dc6044eadb85ead3216
Sam Elliott <soe1@uakron.edu>
2023-07-20 23:10:11 -0400

    working on the api

---
+++ api/QuotesApi/QuotesApi/Controllers/QuotesAppController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuotesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesAppController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public QuotesAppController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetQuotes")]
+        public JsonResult GetQuotes()
+        {
+            string query = "SELECT * FROM abby_quotes";
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("quotesAppDBConn");
+
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource)) 
+            { 
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ api/QuotesApi/QuotesApi/Program.cs
@@ -0,0 +1,34 @@
+using Newtonsoft.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//JSON Serializer
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+var app = builder.Build();
+
+//enable CORS so we can use the services from the front end
+// generally we shouldn't do this in Prod as we really should be whitelisting specific servers as needed 
+
+app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
